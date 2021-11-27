d21ee0fd1c35d7e6a3c431e290aaa3e107e2a91d
Tatev Avoyan <tatev.avoyan@yahoo.com>
2021-11-27 15:33:03 +0400

    Guess the number

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ import random
 
 def guessNum():
     num = random.randrange(1, 15)
-    print(num)
     guess = int(input("Guess the number: "))
     count = 0
 
     while count < 100:
         count += 1
         if num == guess:
-            print("Congratulations you did it in ", count, " try")
+            print(f"Congratulations you did it in {count} try")
             break
         if num < guess:
             print("You guessed too high!")
