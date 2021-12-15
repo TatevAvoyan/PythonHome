fb5e5f1df4f67eb4083b05d1675cc200b4eb76fc
Tatev Avoyan <tatev.avoyan@yahoo.com>
2021-12-15 20:23:15 +0400

    print v2

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 def my_print(*params, end="\n"):
-    text = ''
-    for i in params:
-        text += str(i)
-        text += ' '
-    print(text, end=end)
+    print(*params, end=end)
 
 
 my_print(324, 'Hello', 35, True, "HI", end=" ====== ")
@@ -14,3 +10,6 @@ my_print(324)
 print(324, 'Hello', 35, True, "HI", end=" ====== ")
 print(324, 'Hello', 35, True, "HI", end="\t")
 print(324)
+
+
+
