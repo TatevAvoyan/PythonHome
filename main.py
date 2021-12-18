4988c63188638693f2dc049514e88f5d504367a8
Tatev Avoyan <tatev.avoyan@yahoo.com>
2021-12-18 22:30:36 +0400

    Added lines in list with while loop, Readline

--- main.py
+++ main.py
@@ -1,36 +1,10 @@
-import random
+data = []
 
+with open('names.txt', 'r', encoding='utf-8') as f:
 
-def get_random_word():
-    languages = ["Python", "Go", "JavaScript", "PHP", "Perl", "Swift", "Flask"]
-    latter = random.randrange(0, len(languages))
+    while '' not in data:
+        data.append(f.readline().rstrip('\n'))
 
-    return languages[latter].lower()
+    data.remove('')
 
-
-def guess_word(count, word):
-    user = input("Input Your name: ")
-
-    while True:
-        if count < 1:
-            print(f"{user}, You lost")
-            break
-
-        print(word)
-        print(f"{user} You have {count} attempts")
-        guess = input("Guess the word or letter: ").lower()
-
-        if guess in word:
-            if guess == word:
-                print(f"Congratulations {user} You win!")
-                break
-            print("There is such a symbol in the word")
-        else:
-            count -= 1
-            print(f"Wrong word/latter")
-
-
-rand_count = 3
-rand_word = get_random_word()
-
-guess_word(rand_count, rand_word)
+print(data)
