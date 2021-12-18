b223306208b229cdfe906df5ec93426bbf90f09f
Tatev Avoyan <tatev.avoyan@yahoo.com>
2021-12-18 11:52:02 +0400

    Guess the word

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
-def my_print(*params, end="\n"):
-    print(*params, end=end)
+import random
 
 
-my_print(324, 'Hello', 35, True, "HI", end=" ====== ")
-my_print(324, 'Hello', 35, True, "HI", end="\t")
-my_print(324)
+def get_random_word():
+    languages = ["Python", "Go", "JavaScript", "PHP", "Perl", "Swift", "Flask"]
+    latter = random.randrange(0, len(languages))
+
+    return languages[latter].lower()
 
 
-print(324, 'Hello', 35, True, "HI", end=" ====== ")
-print(324, 'Hello', 35, True, "HI", end="\t")
-print(324)
+def guess_word(count, word):
+    user = input("Input Your name: ")
+
+    while True:
+        if count < 1:
+            print(f"{user}, You lost")
+            break
+
+        print(word)
+        print(f"{user} You have {count} attempts")
+        guess = input("Guess the word or letter: ").lower()
+
+        if guess in word:
+            if guess == word:
+                print(f"Congratulations {user} You win!")
+                break
+            print("There is such a symbol in the word")
+        else:
+            count -= 1
+            print(f"Wrong word/latter")
 
 
+rand_count = 3
+rand_word = get_random_word()
 
+guess_word(rand_count, rand_word)
