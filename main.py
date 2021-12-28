fb3af2e257ed4c4edb77197c5f6aed6bbf36b0b7
Tatev Avoyan <tatev.avoyan@yahoo.com>
2021-12-28 22:07:17 +0400

    Calculator Logging

--- main.py
+++ main.py
@@ -1,12 +1,47 @@
-import os
-import csv
+import logging
+
+logging.basicConfig(filename='mylog.log', level=logging.INFO, format='%(asctime)s %(levelname)s %(funcName)s : %(message)s')
 
 
-# os.rmdir('dir')
-if not os.path.isdir("dir"):
-    os.mkdir("dir")
+def count_the_numbers():
+
+    num1 = input('Input first number: ')
+    char = input('Input operation (+, -, * or /):')
+    num2 = input('Input second number: ')
+    logging.info(f'First number: {num1}, Second number: {num2}, Operator: {char}')
+
+    if not num1.isdigit() or not num2.isdigit():
+        print(f'ValueError: could not convert string to int/float: First number - {num1}, Second number - {num2}')
+        logging.error(f'ValueError: could not convert string to float: First number - {num1}, Second number - {num2}')
+        return 'ValueError'
+    elif num1.isdigit() and num2.isdigit():
+        if char == '+':
+            result = float(num1) + float(num2)
+            logging.info(f'result: {result}')
+            return result
+        elif char == '-':
+            result = float(num1) - float(num2)
+            logging.info(f'result: {result}')
+            return result
+        elif char == '*':
+            result = float(num1) * float(num2)
+            logging.info(f'result: {result}')
+            return result
+        elif char == '/':
+            if not int(num2) == 0:
+                result = float(num1) / float(num2)
+                logging.info(f'result: {result}')
+                return result
+            else:
+                error = 'Cannot divide by zero'
+                logging.error('Division by zero')
+                return error
+        else:
+            error = f'Your operation input is "{char}", pleas input (+, -, * or /)'
+            logging.error(f'Invalid operator: {char}')
+            return error
 
 
-with open('dir/test.csv', 'a+', encoding='utf-8') as f:
-    writer = csv.writer(f, delimiter=',')
-    writer.writerow(['John', 'Smith', 24])
+res = count_the_numbers()
+print(res)
+
