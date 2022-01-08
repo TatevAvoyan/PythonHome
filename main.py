1b80f30aecf35a945bd7f0422552966138a11d62
Tatev Avoyan <tatev.avoyan@yahoo.com>
2022-01-08 16:28:57 +0400

    added dict in .json file

--- main.py
+++ main.py
@@ -1,47 +1,26 @@
-import logging
-
-logging.basicConfig(filename='mylog.log', level=logging.INFO, format='%(asctime)s %(levelname)s %(funcName)s : %(message)s')
+import json
 
 
-def count_the_numbers():
+def data():
 
-    num1 = input('Input first number: ')
-    char = input('Input operation (+, -, * or /): ')
-    num2 = input('Input second number: ')
-    logging.info(f'First number: {num1}, Second number: {num2}, Operator: {char}')
+    name = input("Input Your firstname: ")
+    surname = input("Input Your lastname: ")
+    address = input("Input Your Address: ")
+    age = input("Input Your age: ")
 
-    if not num1.isdigit() or not num2.isdigit():
-        error = f'ValueError: could not convert string to int/float: First number - {num1}, Second number - {num2}'
-        logging.error(error)
-        return error
-    elif num1.isdigit() and num2.isdigit():
-        num1, num2 = int(num1), int(num2)
-        if char == '+':
-            result = num1 + num2
-            logging.info(f'result: {result}')
-            return result
-        elif char == '-':
-            result = num1 - num2
-            logging.info(f'result: {result}')
-            return result
-        elif char == '*':
-            result = num1 * num2
-            logging.info(f'result: {result}')
-            return result
-        elif char == '/':
-            if not num2 == 0:
-                result = num1 / num2
-                logging.info(f'result: {result}')
-                return result
-            else:
-                error = 'Cannot divide by zero'
-                logging.error('Division by zero')
-                return error
-        else:
-            error = f'Your operation input is "{char}", pleas input (+, -, * or /)'
-            logging.error(f'Invalid operator: {char}')
-            return error
+    while not age.isdigit():
+        user_age = input("Input correct value (age): ")
+
+    return {
+        'name': name,
+        'surname': surname,
+        'address': address,
+        'age': age
+    }
 
 
-res = count_the_numbers()
-print(res)
+with open('data_file.json', 'a+', encoding='utf-8') as f:
+    lis = []
+    for i in range(4):
+        lis.append(data())
+    f.write(json.dumps(lis, indent=4))
